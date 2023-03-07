dd3166fa0efbf1dd332feddda1859b2c20a53e0c
GuzeevZhenya <shyst.evgeny@mail.ru>
2023-03-07 18:07:00 +0300

    done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,29 @@
-import {UserType} from '../HW8'
+import {UserType} from '../HW8';
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | {type: 'sort'; payload: 'up' | 'down'}
+  | {type: 'check'; payload: number};
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: Array<UserType>,
+  action: ActionType
+): Array<UserType> => {
+  // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      const NewState = [
+        ...state.sort((a, b) => {
+          if (a.name > b.name) return 1;
+          else if (a.name < b.name) return -1;
+          else return 0;
+        }),
+      ];
+      return action.payload === 'up' ? NewState : NewState.reverse(); // need to fix
     }
-}
+    case 'check': {
+      return [...state.filter((item) => item.age > action.payload)];
+    }
+    default:
+      return state;
+  }
+};
